d0e77070b214c064a64c19ef3b3b6dd9ae9229fa
Mart Hidding <Marthidding@gmail.com>
2018-01-09 13:33:40 +0100

    Add project files.

---
+++ DMA_Service/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DMA_Service.Startup))]
+
+namespace DMA_Service
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
